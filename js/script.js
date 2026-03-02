01f629ed6db6dd153764b5ae72eb6ea79df3cd9c
Kirtan Pradhan <kirtanpradhan666@gmail.com>
2026-03-02 23:30:53 +0530

    Fix js folder: convert from submodule to normal directory

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// examine the document object
+
+<<<<<<< HEAD
+// console.log(document);
+// console.log(document.title);
+// document.title = 'hello';
+// console.log(document.head);
+// console.log(document.forms);
+console.log(document.getElementById('lol'));
+const blogTitle = document.getElementById('lol');
+console.log(blogTitle);
+=======
+console.log('version 3')     ;
+
+
